3b07f71f038812cbc9ee0f572e30159e506c1fb5
Ryomi-j <ryomi.j78@gmail.com>
2023-04-10 15:54:16 +0900

    make name chip

--- src/List/PoketCard.tsx
+++ src/List/PoketCard.tsx
@@ -1,16 +1,28 @@
 import styled from "@emotion/styled";
+import { PoketNameChip } from "../Common/PoketNameChip";
 
 export const PoketCard = () => {
-	return <Item></Item>;
+	return (
+		<Item>
+			<Header>
+				<PoketNameChip />
+			</Header>
+		</Item>
+	);
 };
 
 const Item = styled.li`
-    display: flex;
-    flex-direction: column;
-    padding: 8px;
+	display: flex;
+	flex-direction: column;
+	padding: 8px;
 
 	width: 250px;
 	height: 300px;
 	border: 1px solid #c0c0c0;
 	box-shadow: 1px 1px 3px 1px #c0c0c0;
 `;
+
+const Header = styled.section`
+	display: flex;
+`;
+
