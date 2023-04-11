021acbca0e6f6931450a1f0dc2ea5b071bddeb5a
Ryomi-j <ryomi.j78@gmail.com>
2023-04-11 11:42:15 +0900

    optimize getting data that getting data that already exist

--- src/Store/pokemonDetailSlice.ts
+++ src/Store/pokemonDetailSlice.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { fetchPokemonsDetailAPI, PokemonDetailType } from "../SVC/PokemonService";
+import { RootState } from ".";
 
 // First, create the thunk
 export const fetchPokemonDetail = createAsyncThunk(
@@ -7,6 +8,13 @@ export const fetchPokemonDetail = createAsyncThunk(
     async (name: string) => {
 	const response = await fetchPokemonsDetailAPI(name);
 	return response;
+}, {
+    // 스크롤을 다시 올렸을 때, 기존을 데이터를 중복해서 받아오지 않게 함
+    condition: (name, {getState}) => {
+        const {pokemonDetail} = getState() as RootState
+        const pokemon = pokemonDetail.pokemonDetails[name]
+        return !pokemon
+    }
 }
 );
 
